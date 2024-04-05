23ffcb85adaa35e56bcc82917e253188e6a9de02
Thisari2002 <amandathisari2002@gmail.com>
2024-04-05 10:12:26 +0530

    create and run the surver

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Payment from "./pages/Payment"
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/payment" element={<Payment    />}  />
+      </Routes>
+    </BrowserRouter>
   )
 }
