049af67a98db6105f11df747f85085596292e9f7
Thisari2002 <amandathisari2002@gmail.com>
2024-04-11 21:25:10 +0530

    create payment model

---
+++ api/models/payment.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema({
+    customerID:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    transactionID:{
+        type:String,
+        require:true,
+        uniqued:true,
+    },
+    paymentAmount:{
+        type:String,
+        required:true,
+    },
+    paymentDate:{
+        type:String,
+        required:true,
+    },
+},{timestamps:true});
+
+const Payment=mongoose.model('Payment',paymentSchema);
+
+export default Payment;
